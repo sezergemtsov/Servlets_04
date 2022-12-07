c79046590e711d1d3dd3b87a3b0f6181f6c72aa6
sezergemtsov <sezer.gemtsov@ya.ru>
2022-12-07 16:10:23 +0300

    mark to delete v.1

--- src/main/java/ru/netology/repository/PostRepositoryStubImpl.java
+++ src/main/java/ru/netology/repository/PostRepositoryStubImpl.java
@@ -2,6 +2,7 @@ package ru.netology.repository;
 
 import org.springframework.stereotype.Repository;
 import ru.netology.exception.NotFoundException;
+import ru.netology.model.MetaPost;
 import ru.netology.model.Post;
 
 import java.util.*;
@@ -11,37 +12,43 @@ import java.util.concurrent.atomic.AtomicLong;
 // Stub
 @Repository
 public class PostRepositoryStubImpl implements PostRepository {
-    private ConcurrentHashMap<Long, Post> repository = new ConcurrentHashMap();
+    private ConcurrentHashMap<Long, MetaPost> repository = new ConcurrentHashMap<>();
     private static AtomicLong idCounter = new AtomicLong(0);
 
     public List<Post> all() {
         List<Post> posts = new Vector<>();
-        repository.entrySet().forEach(x -> posts.add(x.getValue()));
+        repository.entrySet().stream()
+                .filter(x->x.getValue().isNotDeleted())
+                .forEach(x->posts.add(x.getValue().getPost()));
         return posts;
     }
 
     public Optional<Post> getById(long id) throws NotFoundException {
         if (repository.containsKey(id)) {
-            return Optional.ofNullable(repository.get(id));
+            return Optional.ofNullable(repository.get(id).getPost());
         } else {
             throw new NotFoundException("Repository doesn't contain current post");
         }
     }
 
-    public Post save(Post post) {
+    public Post save(Post post) throws NotFoundException {
         //Если пришел пост с id = 0, регестрируем новый номер id поста в зависимости от существующих в репозитории
         //Будем считать что удаленные номера можно использовать повторно
         if (post.getId() == 0) {
             post.setId(idCounter.incrementAndGet());
+            repository.get(post.getId()).setPost(post);
+        } else if (repository.containsKey(post.getId())) {
+            repository.get(post.getId()).setPost(post);
+            //Если пост с id!=0 не найден в репозитории будем возвращать 404
+        } else {
+            throw new NotFoundException();
         }
-        //Если пост с id!=0 не найден в репозитории будем просто сохранять его под своим номером
-        repository.put(post.getId(), post);
         return post;
     }
 
     public void removeById(long id) throws NotFoundException {
         if (repository.containsKey(id)) {
-            repository.remove(id);
+            repository.get(id).markToDelete();
         } else {
             throw new NotFoundException("Nothing to delete. Repository doesn't contain current post");
         }
